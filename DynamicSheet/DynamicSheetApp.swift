98b6a17f16b7994b61ee56af6df8768a8d4ea6cd
gunnargray <hi@gunnargray.com>
2025-07-15 16:22:26 -0500

    Initial Commit

---
+++ DynamicSheet/DynamicSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicSheetApp.swift
+//  DynamicSheet
+//
+//  Created by Gunnar Gray on 7/15/25.
+//
+
+import SwiftUI
+
+@main
+struct DynamicSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
